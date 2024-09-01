e49aac3db785ed2796c75f4a131a5b011e9f7fff
sayurelektronik <sayurelektronik@gmail.com>
2024-09-01 10:10:28 +0700

    create create post

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Create Post</h1>
+
+    <form action="{{ route('posts.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+            @error('title')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="content">Content</label>
+            <textarea class="form-control" id="content" name="content">{{ old('content') }}</textarea>
+            @error('content')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">Create</button>
+    </form>
+@endsection
